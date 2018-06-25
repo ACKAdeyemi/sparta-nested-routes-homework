226bbe873efdbdedfff6dc0b561b80ce4cef8f40
ACKAdeyemi <akola-adeyemi@spartaglobal.com>
2018-06-25 05:19:57 +0100

    Homework task complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,30 +5,36 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    @user = User.find(params[:user_id])
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @user = User.find(params[:user_id])
   end
 
   # GET /posts/new
   def new
+    @user = User.find(params[:user_id])
     @post = Post.new
   end
 
   # GET /posts/1/edit
   def edit
+    @user = User.find(params[:user_id])
   end
 
   # POST /posts
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    @user = User.find(params[:user_id])
+    @post.user = @user
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to user_url(@user), notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -40,9 +46,11 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+    @user = User.find(params[:user_id])
+
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to user_url(@user), notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -54,9 +62,11 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
+    @user = User.find(params[:user_id])
+
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to user_url(@user), notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
